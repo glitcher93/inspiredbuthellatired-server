17abc6a5b20a097c5759edd32b3e3d4145b998b8
glitcher93 <nigeljdsouza@gmail.com>
2023-03-19 12:12:35 -0400

    created IRequest

--- utils/interfaces.ts
+++ utils/interfaces.ts
@@ -1,4 +1,10 @@
+import { Request } from 'express';
+import { JwtPayload } from "jsonwebtoken"
 
 export interface IFile {
     mimetype: string
+}
+
+export interface IRequest extends Request {
+    payload?: string | JwtPayload
 }
