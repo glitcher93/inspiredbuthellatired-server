a9df176e82130e27dff966f500f09e7cd021ab6f
glitcher93 <nigeljdsouza@gmail.com>
2023-03-20 22:20:28 -0400

    imported products route

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import products from './routes/products';
 
 dotenv.config();
 
@@ -10,5 +11,7 @@ const PORT = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
+
+app.use('/products', products);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
