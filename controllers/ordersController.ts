d182f3a9ef99433467dc0c79fb006b42915afbfc
glitcher93 <nigeljdsouza@gmail.com>
2023-03-27 17:11:36 -0400

    began creating order controller

---
+++ controllers/ordersController.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express';
+import db from '../db';
+
+export const getAllOrders = async (req: Request, res: Response) => {
+    try {
+        const orders = await db('orders');
+        return res.status(200).json(orders);
+    } catch {
+        return res.status(400).json({error: 'error getting orders'})
+    }
+}
+
+export const patchOrder = async (req: Request, res: Response) => {
+    const { id, trackingNumber } = req.params;
+
+    try {
+        await db('orders')
+                .where({id})
+                .update({trackingNumber});
+        return res.status(200).json({
+            message: 'order updated',
+        });
+    } catch ({ message }) {
+        return res.status(400).json({error: message})
+    }
+}
--- dist/controllers/ordersController.js
+++ dist/controllers/ordersController.js
@@ -1,1 +1,41 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.patchOrder = exports.getAllOrders = void 0;
+const db_1 = __importDefault(require("../db"));
+const getAllOrders = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const orders = yield (0, db_1.default)('orders');
+        return res.status(200).json(orders);
+    }
+    catch (_a) {
+        return res.status(400).json({ error: 'error getting orders' });
+    }
+});
+exports.getAllOrders = getAllOrders;
+const patchOrder = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { id, trackingNumber } = req.params;
+    try {
+        yield (0, db_1.default)('orders')
+            .where({ id })
+            .update({ trackingNumber });
+        return res.status(200).json({
+            message: 'order updated',
+        });
+    }
+    catch ({ message }) {
+        return res.status(400).json({ error: message });
+    }
+});
+exports.patchOrder = patchOrder;
