4e07d72044bb6ff7189dfb17526cf32b1c687d7b
glitcher93 <nigeljdsouza@gmail.com>
2023-03-27 17:06:31 -0400

    created controller functions for products route

--- controllers/productsController.ts
+++ controllers/productsController.ts
@@ -1,35 +1,39 @@
 import db from '../db';
 import { Request, Response } from "express";
 
-export const getAllProducts = (req: Request, res: Response) => {
-    db('products')
-        .then(products => {
-            res.status(200).json(products)
-        })
-        .catch(err => {
-            res.status(400).json({error: 'error getting products'})
-        })
+export const getAllProducts = async (req: Request, res: Response) => {
+    try {
+        const products = await db('products');
+        return res.status(200).json(products);
+    } catch {
+        res.status(400).json({error: 'error getting products'})
+    }
 }
 
-export const getPaintings = (req: Request, res: Response) => {
-    db('products')
-        .where({type: "Painting"})
-        .then(products => {
-            res.status(200).json(products);
-        })
-        .catch(err => {
-            res.status(400).json({error: 'error getting products'})
-        })
+export const getPaintings = async (req: Request, res: Response) => {
+    try {
+        const products = await db('products').where({type: "Painting"});
+        return res.status(200).json(products);
+    } catch {
+        res.status(400).json({error: 'error getting products'})
+    }
 }
 
-export const getPrints = (req: Request, res: Response) => {
-    db('products')
-        .where({type: "Print"})
-        .then(products => {
-            res.status(200).json(products);
-        })
-        .catch(err => {
-            res.status(400).json({error: 'error getting products'})
-        })
+export const getPrints = async (req: Request, res: Response) => {
+    try {
+        const products = await db('products').where({type: "Print"});
+        return res.status(200).json(products);
+    } catch {
+        res.status(400).json({error: 'error getting products'})
+    }
+}
+
+export const getProductsAdmin = async (req: Request, res: Response) => {
+    try {
+        const products = await db('products');
+        return res.status(200).json(products);
+    } catch {
+        res.status(400).json({error: 'error getting products'})
+    }
 }
 
--- dist/controllers/productsController.js
+++ dist/controllers/productsController.js
@@ -1,39 +1,56 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getPrints = exports.getPaintings = exports.getAllProducts = void 0;
+exports.getProductsAdmin = exports.getPrints = exports.getPaintings = exports.getAllProducts = void 0;
 const db_1 = __importDefault(require("../db"));
-const getAllProducts = (req, res) => {
-    (0, db_1.default)('products')
-        .then(products => {
-        res.status(200).json(products);
-    })
-        .catch(err => {
+const getAllProducts = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const products = yield (0, db_1.default)('products');
+        return res.status(200).json(products);
+    }
+    catch (_a) {
         res.status(400).json({ error: 'error getting products' });
-    });
-};
+    }
+});
 exports.getAllProducts = getAllProducts;
-const getPaintings = (req, res) => {
-    (0, db_1.default)('products')
-        .where({ type: "Painting" })
-        .then(products => {
-        res.status(200).json(products);
-    })
-        .catch(err => {
+const getPaintings = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const products = yield (0, db_1.default)('products').where({ type: "Painting" });
+        return res.status(200).json(products);
+    }
+    catch (_b) {
         res.status(400).json({ error: 'error getting products' });
-    });
-};
+    }
+});
 exports.getPaintings = getPaintings;
-const getPrints = (req, res) => {
-    (0, db_1.default)('products')
-        .where({ type: "Print" })
-        .then(products => {
-        res.status(200).json(products);
-    })
-        .catch(err => {
+const getPrints = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const products = yield (0, db_1.default)('products').where({ type: "Print" });
+        return res.status(200).json(products);
+    }
+    catch (_c) {
         res.status(400).json({ error: 'error getting products' });
-    });
-};
+    }
+});
 exports.getPrints = getPrints;
+const getProductsAdmin = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const products = yield (0, db_1.default)('products');
+        return res.status(200).json(products);
+    }
+    catch (_d) {
+        res.status(400).json({ error: 'error getting products' });
+    }
+});
+exports.getProductsAdmin = getProductsAdmin;
