fb78a336779921607291f7c27e011e5246f5b1d3
glitcher93 <nigeljdsouza@gmail.com>
2023-03-30 14:51:53 -0400

    updated getAllOrders to respond with all necessary items

--- controllers/ordersController.ts
+++ controllers/ordersController.ts
@@ -1,10 +1,59 @@
 import { Request, Response } from 'express';
 import db from '../db';
+import { IItem, IItemHash } from '../utils/interfaces';
 
 export const getAllOrders = async (req: Request, res: Response) => {
     try {
         const orders = await db('orders');
-        return res.status(200).json(orders);
+        const products = await db('products');
+        const formattedOrders = orders.map((order) => {
+            const formattedProducts: IItem[] = [];
+            const formattedOrder = {
+                id: order.id,
+                orderId: order.orderId,
+                subtotal: order.subtotal,
+                total: order.total,
+                shippingInfo: {
+                    name: order.name,
+                    addressLineOne: order.addressLineOne,
+                    addressLineTwo: order.addressLineTwo || null,
+                    city: order.city,
+                    state: order.state,
+                    phoneNumber: order.phoneNumber,
+                    paymentStatus: order.paymentStatus,
+                    trackingNumber: order.trackingNumber || null,
+                    createdAt: order.createdAt
+                },
+                items: formattedProducts
+            }
+            const itemHash: IItemHash = {};
+            let itemQuantity: number;
+            order.products.forEach((productId: string, index: number) => {
+                const foundProduct = products.find((product) => product.id === productId);
+                if (order.quantities[index]) {
+                    itemQuantity = order.quantities[index];
+                }
+                if (foundProduct) {
+                    itemHash[foundProduct.id] = foundProduct;
+                    const currentProduct = itemHash[foundProduct.id];
+                    
+                    if (currentProduct) {
+                        const formattedProduct = {
+                            id: currentProduct.id,
+                            image: currentProduct.image,
+                            title: currentProduct.title,
+                            priceInCents: currentProduct.priceInCents,
+                            size: currentProduct.size,
+                            type: currentProduct.type,
+                            quantity: itemQuantity
+                        }
+                        formattedProducts.push(formattedProduct);
+                    }
+                }
+            })
+            return formattedOrder;
+        });
+        return res.status(200).json(formattedOrders);
     } catch {
         return res.status(400).json({error: 'Error getting orders'})
     }
--- dist/controllers/ordersController.js
+++ dist/controllers/ordersController.js
@@ -17,7 +17,54 @@ const db_1 = __importDefault(require("../db"));
 const getAllOrders = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const orders = yield (0, db_1.default)('orders');
-        return res.status(200).json(orders);
+        const products = yield (0, db_1.default)('products');
+        const formattedOrders = orders.map((order) => {
+            const formattedProducts = [];
+            const formattedOrder = {
+                id: order.id,
+                orderId: order.orderId,
+                subtotal: order.subtotal,
+                total: order.total,
+                shippingInfo: {
+                    name: order.name,
+                    addressLineOne: order.addressLineOne,
+                    addressLineTwo: order.addressLineTwo || null,
+                    city: order.city,
+                    state: order.state,
+                    phoneNumber: order.phoneNumber,
+                    paymentStatus: order.paymentStatus,
+                    trackingNumber: order.trackingNumber || null,
+                    createdAt: order.createdAt
+                },
+                items: formattedProducts
+            };
+            const itemHash = {};
+            let itemQuantity;
+            order.products.forEach((productId, index) => {
+                const foundProduct = products.find((product) => product.id === productId);
+                if (order.quantities[index]) {
+                    itemQuantity = order.quantities[index];
+                }
+                if (foundProduct) {
+                    itemHash[foundProduct.id] = foundProduct;
+                    const currentProduct = itemHash[foundProduct.id];
+                    if (currentProduct) {
+                        const formattedProduct = {
+                            id: currentProduct.id,
+                            image: currentProduct.image,
+                            title: currentProduct.title,
+                            priceInCents: currentProduct.priceInCents,
+                            size: currentProduct.size,
+                            type: currentProduct.type,
+                            quantity: itemQuantity
+                        };
+                        formattedProducts.push(formattedProduct);
+                    }
+                }
+            });
+            return formattedOrder;
+        });
+        return res.status(200).json(formattedOrders);
     }
     catch (_a) {
         return res.status(400).json({ error: 'Error getting orders' });
