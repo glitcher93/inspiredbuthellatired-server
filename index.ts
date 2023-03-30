d73855136424bdde10649a82a7179eb0e36a8a97
glitcher93 <nigeljdsouza@gmail.com>
2023-03-30 14:57:24 -0400

    imported checkout and orders, updated webhook route

--- index.ts
+++ index.ts
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import products from './routes/products';
 import webhook from './routes/webhook';
+import checkout from './routes/checkout';
+import orders from './routes/orders';
 
 dotenv.config();
 
@@ -10,10 +12,12 @@ const app: Express = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(cors());
-app.use('/webhook', express.raw({type: "*/*"}), webhook);
+app.use('/webhook', express.raw({type: "application/json"}), webhook);
 app.use(express.json());
 app.use(express.static('public'));
 
 app.use('/products', products);
+app.use('/checkout', checkout);
+app.use('/orders', orders)
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
