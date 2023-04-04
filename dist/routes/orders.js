fbafe07e1593b1a032cb5b5bfb8c2e7f230318e5
glitcher93 <nigeljdsouza@gmail.com>
2023-04-04 16:40:28 -0400

    added route to get order by id

--- dist/routes/orders.js
+++ dist/routes/orders.js
@@ -4,8 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const ordersController_1 = require("../controllers/ordersController");
 const auth_1 = __importDefault(require("../middleware/auth"));
+const validOrder_1 = __importDefault(require("../middleware/validOrder"));
 const router = express_1.default.Router();
-router.get('/', auth_1.default);
+router.get('/', auth_1.default, ordersController_1.getAllOrders);
+router.get('/:orderId', validOrder_1.default, ordersController_1.getOrderById);
 router.patch('/:id', auth_1.default);
 exports.default = router;
--- routes/orders.ts
+++ routes/orders.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import { getAllOrders, getOrderById } from '../controllers/ordersController';
 import auth from '../middleware/auth';
+import validOrder from '../middleware/validOrder';
 
 const router = express.Router();
 
-router.get('/', auth);
+router.get('/', auth, getAllOrders);
+
+router.get('/:orderId', validOrder, getOrderById);
 
 router.patch('/:id', auth);
 
