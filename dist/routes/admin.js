c1f438243aee6d8b7d1c8d02537f58608ebc0e3e
glitcher93 <nigeljdsouza@gmail.com>
2023-04-04 16:41:36 -0400

    added possible future routes for admin (commented out for now)

--- dist/routes/admin.js
+++ dist/routes/admin.js
@@ -5,6 +5,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const adminController_1 = require("../controllers/adminController");
+// import { getAllOrders } from '../controllers/ordersController';
+// import { getAllProducts } from '../controllers/productsController';
+// import auth from '../middleware/auth';
 const router = express_1.default.Router();
 router.post('/login', adminController_1.login);
+// router.get('/orders', auth, getAllOrders)
+// router.delete('/delete-order/:id', auth)
+// router.get('/products', auth, getAllProducts)
+// router.post('/new-product', auth)
+// router.put('/edit-product/:id', auth)
+// router.delete('/delete-product/:id', auth)
 exports.default = router;
--- routes/admin.ts
+++ routes/admin.ts
@@ -1,9 +1,24 @@
 import express from 'express';
 import { login } from '../controllers/adminController';
+// import { getAllOrders } from '../controllers/ordersController';
+// import { getAllProducts } from '../controllers/productsController';
+// import auth from '../middleware/auth';
 
 const router = express.Router();
 
 router.post('/login', login);
+
+// router.get('/orders', auth, getAllOrders)
+
+// router.delete('/delete-order/:id', auth)
+
+// router.get('/products', auth, getAllProducts)
+
+// router.post('/new-product', auth)
+
+// router.put('/edit-product/:id', auth)
+
+// router.delete('/delete-product/:id', auth)
 
 export default router;
 
