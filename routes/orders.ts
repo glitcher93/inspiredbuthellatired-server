160ebd11de6a7bfbfc21f3d53524eedb0dcd8df6
glitcher93 <nigeljdsouza@gmail.com>
2023-04-10 15:53:09 -0400

    deleted unnecessary routes

--- routes/orders.ts
+++ routes/orders.ts
@@ -1,14 +1,10 @@
 import express from 'express';
-import { getAllOrders, getOrderById } from '../controllers/ordersController';
-import auth from '../middleware/auth';
+import { getOrderById } from '../controllers/ordersController';
 import validOrder from '../middleware/validOrder';
 
 const router = express.Router();
 
-router.get('/', auth, getAllOrders);
-
 router.get('/:orderId', validOrder, getOrderById);
 
-router.patch('/:id', auth);
 
 export default router;
