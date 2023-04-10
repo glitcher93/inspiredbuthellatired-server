303613404c1870960c30e3bd5547a14fad487767
glitcher93 <nigeljdsouza@gmail.com>
2023-04-10 15:56:43 -0400

    created deleteOrder, made adjustments to all other controller functions

--- controllers/ordersController.ts
+++ controllers/ordersController.ts
@@ -11,6 +11,7 @@ export const getAllOrders = async (req: Request, res: Response) => {
             const formattedOrder = {
                 id: order.id,
                 orderId: order.orderId,
+                paymentStatus: order.paymentStatus,
                 subtotal: order.subtotal,
                 total: order.total,
                 shippingInfo: {
@@ -21,8 +22,8 @@ export const getAllOrders = async (req: Request, res: Response) => {
                     state: order.state,
                     postalCode: order.postalCode,
                     phoneNumber: order.phoneNumber,
-                    paymentStatus: order.paymentStatus,
                     trackingNumber: order.trackingNumber || null,
+                    isFulfilled: order.isFulfilled,
                     createdAt: order.createdAt
                 },
                 items: formattedProducts
@@ -54,9 +55,68 @@ export const getAllOrders = async (req: Request, res: Response) => {
             })
             return formattedOrder;
         });
-        return res.status(200).json(formattedOrders);
+        return res.status(200).json(formattedOrders).end();
     } catch {
-        return res.status(400).json({error: 'Error getting orders'})
+        return res.status(400).json({error: 'Error getting orders'}).end();
+    }
+}
+
+export const getRecentOrders = async (req: Request, res: Response) => {
+    try {
+        const orders = await db('orders');
+        const products = await db('products');
+        const formattedOrders = orders.map((order) => {
+            const formattedProducts: IItem[] = [];
+            const formattedOrder = {
+                id: order.id,
+                orderId: order.orderId,
+                subtotal: order.subtotal,
+                paymentStatus: order.paymentStatus,
+                total: order.total,
+                shippingInfo: {
+                    name: order.name,
+                    addressLineOne: order.addressLineOne,
+                    addressLineTwo: order.addressLineTwo || null,
+                    city: order.city,
+                    state: order.state,
+                    postalCode: order.postalCode,
+                    phoneNumber: order.phoneNumber,
+                    trackingNumber: order.trackingNumber || null,
+                    isFulfilled: order.isFulfilled,
+                    createdAt: order.createdAt
+                },
+                items: formattedProducts
+            }
+            const itemHash: IItemHash = {};
+            let itemQuantity: number;
+            order.products.forEach((productId: string, index: number) => {
+                const foundProduct = products.find((product) => product.id === productId);
+                if (order.quantities[index]) {
+                    itemQuantity = order.quantities[index];
+                }
+                if (foundProduct) {
+                    itemHash[foundProduct.id] = foundProduct;
+                    const currentProduct = itemHash[foundProduct.id];
+                    
+                    if (currentProduct) {
+                        const formattedProduct = {
+                            id: currentProduct.id,
+                            image: currentProduct.image,
+                            title: currentProduct.title,
+                            priceInCents: currentProduct.priceInCents,
+                            size: currentProduct.size,
+                            type: currentProduct.type,
+                            quantity: itemQuantity
+                        }
+                        formattedProducts.push(formattedProduct);
+                    }
+                }
+            })
+            return formattedOrder;
+        });
+        return res.status(200).json(formattedOrders).end();
+    } catch (err) {
+        return res.status(400).send("Error retrieving orders").end()
     }
 }
 
@@ -71,6 +131,7 @@ export const getOrderById = async (req: Request, res: Response) => {
         const formattedOrder = {
             id: order.id,
             orderId: order.orderId,
+            paymentStatus: order.paymentStatus,
             subtotal: order.subtotal,
             total: order.total,
             shippingInfo: {
@@ -81,7 +142,6 @@ export const getOrderById = async (req: Request, res: Response) => {
                 state: order.state,
                 postalCode: order.postalCode,
                 phoneNumber: order.phoneNumber,
-                paymentStatus: order.paymentStatus,
                 trackingNumber: order.trackingNumber || null,
                 createdAt: order.createdAt
             },
@@ -112,23 +172,42 @@ export const getOrderById = async (req: Request, res: Response) => {
                     }
                 }
             })
-        return res.status(200).json(formattedOrder);
+        return res.status(200).json(formattedOrder).end();
     } catch {
-        return res.status(400).json({error: 'Error getting orders'})
+        return res.status(400).json({error: 'Error getting orders'}).end();
     }
 }
 
-export const patchOrder = async (req: Request, res: Response) => {
+export const addTracking = async (req: Request, res: Response) => {
     const { id, trackingNumber } = req.params;
 
     try {
         await db('orders')
                 .where({id})
-                .update({trackingNumber});
-        return res.status(200).json({
-            message: 'order updated',
-        });
+                .update({
+                    trackingNumber,
+                    isFulfilled: true
+                });
+        return res.status(200).json("Order updated").end();
     } catch (err) {
-        return res.status(400).json({err})
+        return res.status(400).json({err}).end();
+    }
+}
+
+export const deleteOrder = async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    try {
+        const orders = await db('orders').where({id})
+        const order = orders[0]
+
+        if (order.trackingNumber && order.isFulfilled) {
+            await db('orders').delete().where({id})
+            return res.status(200).send("Order deleted").end();
+        } else {
+            return res.status(404).send("Order does not have a tracking number and is not fulfilled").end();
+        }
+    } catch (err) {
+        return res.status(404).send("Order not found").end()
     }
 }
--- dist/controllers/ordersController.js
+++ dist/controllers/ordersController.js
@@ -12,7 +12,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.patchOrder = exports.getOrderById = exports.getAllOrders = void 0;
+exports.deleteOrder = exports.addTracking = exports.getOrderById = exports.getRecentOrders = exports.getAllOrders = void 0;
 const db_1 = __importDefault(require("../db"));
 const getAllOrders = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
@@ -23,6 +23,7 @@ const getAllOrders = (req, res) => __awaiter(void 0, void 0, void 0, function* (
             const formattedOrder = {
                 id: order.id,
                 orderId: order.orderId,
+                paymentStatus: order.paymentStatus,
                 subtotal: order.subtotal,
                 total: order.total,
                 shippingInfo: {
@@ -33,8 +34,8 @@ const getAllOrders = (req, res) => __awaiter(void 0, void 0, void 0, function* (
                     state: order.state,
                     postalCode: order.postalCode,
                     phoneNumber: order.phoneNumber,
-                    paymentStatus: order.paymentStatus,
                     trackingNumber: order.trackingNumber || null,
+                    isFulfilled: order.isFulfilled,
                     createdAt: order.createdAt
                 },
                 items: formattedProducts
@@ -65,13 +66,72 @@ const getAllOrders = (req, res) => __awaiter(void 0, void 0, void 0, function* (
             });
             return formattedOrder;
         });
-        return res.status(200).json(formattedOrders);
+        return res.status(200).json(formattedOrders).end();
     }
     catch (_a) {
-        return res.status(400).json({ error: 'Error getting orders' });
+        return res.status(400).json({ error: 'Error getting orders' }).end();
     }
 });
 exports.getAllOrders = getAllOrders;
+const getRecentOrders = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const orders = yield (0, db_1.default)('orders');
+        const products = yield (0, db_1.default)('products');
+        const formattedOrders = orders.map((order) => {
+            const formattedProducts = [];
+            const formattedOrder = {
+                id: order.id,
+                orderId: order.orderId,
+                subtotal: order.subtotal,
+                paymentStatus: order.paymentStatus,
+                total: order.total,
+                shippingInfo: {
+                    name: order.name,
+                    addressLineOne: order.addressLineOne,
+                    addressLineTwo: order.addressLineTwo || null,
+                    city: order.city,
+                    state: order.state,
+                    postalCode: order.postalCode,
+                    phoneNumber: order.phoneNumber,
+                    trackingNumber: order.trackingNumber || null,
+                    isFulfilled: order.isFulfilled,
+                    createdAt: order.createdAt
+                },
+                items: formattedProducts
+            };
+            const itemHash = {};
+            let itemQuantity;
+            order.products.forEach((productId, index) => {
+                const foundProduct = products.find((product) => product.id === productId);
+                if (order.quantities[index]) {
+                    itemQuantity = order.quantities[index];
+                }
+                if (foundProduct) {
+                    itemHash[foundProduct.id] = foundProduct;
+                    const currentProduct = itemHash[foundProduct.id];
+                    if (currentProduct) {
+                        const formattedProduct = {
+                            id: currentProduct.id,
+                            image: currentProduct.image,
+                            title: currentProduct.title,
+                            priceInCents: currentProduct.priceInCents,
+                            size: currentProduct.size,
+                            type: currentProduct.type,
+                            quantity: itemQuantity
+                        };
+                        formattedProducts.push(formattedProduct);
+                    }
+                }
+            });
+            return formattedOrder;
+        });
+        return res.status(200).json(formattedOrders).end();
+    }
+    catch (err) {
+        return res.status(400).send("Error retrieving orders").end();
+    }
+});
+exports.getRecentOrders = getRecentOrders;
 const getOrderById = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { orderId } = req.params;
     try {
@@ -82,6 +142,7 @@ const getOrderById = (req, res) => __awaiter(void 0, void 0, void 0, function* (
         const formattedOrder = {
             id: order.id,
             orderId: order.orderId,
+            paymentStatus: order.paymentStatus,
             subtotal: order.subtotal,
             total: order.total,
             shippingInfo: {
@@ -92,7 +153,6 @@ const getOrderById = (req, res) => __awaiter(void 0, void 0, void 0, function* (
                 state: order.state,
                 postalCode: order.postalCode,
                 phoneNumber: order.phoneNumber,
-                paymentStatus: order.paymentStatus,
                 trackingNumber: order.trackingNumber || null,
                 createdAt: order.createdAt
             },
@@ -122,25 +182,44 @@ const getOrderById = (req, res) => __awaiter(void 0, void 0, void 0, function* (
                 }
             }
         });
-        return res.status(200).json(formattedOrder);
+        return res.status(200).json(formattedOrder).end();
     }
     catch (_b) {
-        return res.status(400).json({ error: 'Error getting orders' });
+        return res.status(400).json({ error: 'Error getting orders' }).end();
     }
 });
 exports.getOrderById = getOrderById;
-const patchOrder = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+const addTracking = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { id, trackingNumber } = req.params;
     try {
         yield (0, db_1.default)('orders')
             .where({ id })
-            .update({ trackingNumber });
-        return res.status(200).json({
-            message: 'order updated',
+            .update({
+            trackingNumber,
+            isFulfilled: true
         });
+        return res.status(200).json("Order updated").end();
     }
     catch (err) {
-        return res.status(400).json({ err });
+        return res.status(400).json({ err }).end();
     }
 });
-exports.patchOrder = patchOrder;
+exports.addTracking = addTracking;
+const deleteOrder = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { id } = req.params;
+    try {
+        const orders = yield (0, db_1.default)('orders').where({ id });
+        const order = orders[0];
+        if (order.trackingNumber && order.isFulfilled) {
+            yield (0, db_1.default)('orders').delete().where({ id });
+            return res.status(200).send("Order deleted").end();
+        }
+        else {
+            return res.status(404).send("Order does not have a tracking number and is not fulfilled").end();
+        }
+    }
+    catch (err) {
+        return res.status(404).send("Order not found").end();
+    }
+});
+exports.deleteOrder = deleteOrder;
