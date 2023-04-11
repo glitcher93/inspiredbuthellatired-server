3064e297004b27d0f04cdf244344532101011268
glitcher93 <nigeljdsouza@gmail.com>
2023-04-11 18:23:17 -0400

    created addProduct, editProduct, and deleteProduct functions

--- controllers/productsController.ts
+++ controllers/productsController.ts
@@ -1,5 +1,12 @@
 import db from '../db';
 import { Request, Response } from "express";
+import dotenv from 'dotenv';
+import fs from 'fs';
+import path from 'path';
+
+dotenv.config();
+
+const serverURL = process.env.SERVER_URL || 'http://localhost:8080';
 
 export const getAllProducts = async (req: Request, res: Response) => {
     try {
@@ -34,6 +41,108 @@ export const getRandomProducts = async (req: Request, res: Response) => {
         return res.status(200).json(products);
     } catch {
         res.status(400).json({error: 'error getting products'})
+    }
+}
+
+export const addProduct = async (req: Request, res: Response) => {
+    const { title, size, price, type } = req.body;
+
+    console.log(req.file)
+
+    const newProduct = {
+        title,
+        image: `${serverURL}/images/${req.file!.filename.replace(path.extname(req.file!.filename), '.webp')}`,
+        size,
+        priceInCents: price * 100,
+        type
+    }
+
+    try {
+        const products = await db("products");
+
+        const foundProduct = products.find(product => product.title === title);
+
+        if (!foundProduct) {
+            await db("products").insert(newProduct);
+            return res.status(201).send("Product created!")
+        } else {
+            return res.status(400).send("Error saving product");
+        }
+    } catch {
+        return res.status(400).send("Error saving product");
+    }
+}
+
+export const editProduct = async (req: Request, res: Response) => {
+    const { title, size, price, type, inStock } = req.body;
+    const { id } = req.params;
+
+    try {
+        const product = await db("products").where({id})
+
+        const foundProduct = product[0];
+
+        if (req.file! && foundProduct.image.startsWith(serverURL)) {
+            fs.unlink("public" + foundProduct.image.replace(serverURL, ''), (err) => {
+                if (err) {
+                    console.log(err)
+                }
+            });
+        }
+
+        const editedProduct = {
+            title: title && title !== foundProduct.title ? title : foundProduct.title,
+            image: req.file! ? `${serverURL}/images/${req.file.filename.replace(path.extname(req.file.filename), '.webp')}` : foundProduct.image,
+            size: size && size !== foundProduct.size ? size : foundProduct.size,
+            priceInCents: price && price !== Number(foundProduct.priceInCents) ? Number(price) * 100 : Number(foundProduct.priceInCents),
+            type: type && type !== foundProduct.type ? type : foundProduct.type,
+            inStock: inStock && inStock !== foundProduct.inStock ? inStock : foundProduct.inStock
+        }
+
+        console.log(editedProduct)
+
+        await db("products").update(editedProduct).where({id});
+        return res.status(200).send("Product Edited!")
+    } catch (e: any) {
+        return res.status(404).send("Product not found.")
+    }
+    
+}
+
+export const deleteProduct = async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    try {
+        const product = await db("products").where({id})
+
+        const foundProduct = product[0];
+
+        const orders = await db('orders');
+
+        const productExistsInOrder: boolean[] = [];
+
+        orders.forEach((order) => {
+            if (order.products.includes(foundProduct.id)) {
+                productExistsInOrder.push(true)
+            } else {
+                productExistsInOrder.push(false)
+            }
+        })
+
+        if (productExistsInOrder.some((el) => el === true)) {
+            return res.status(400).send("Cannot delete product: The product exists in one or more order. The order(s) must be fulfilled and deleted before you can delete this product.")
+        } else {
+            fs.unlink("public" + foundProduct.image.replace(serverURL, ''), (err) => {
+                if (err) {
+                    console.log(err)
+                }
+            });
+            await db('products').delete().where({id})
+            return res.status(200).send("Product successfully deleted.")
+        }
+
+    } catch (e: any) {
+        return res.status(404).send("Product not found.")
     }
 }
 
--- dist/controllers/productsController.js
+++ dist/controllers/productsController.js
@@ -12,8 +12,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getRandomProducts = exports.getPrints = exports.getPaintings = exports.getAllProducts = void 0;
+exports.deleteProduct = exports.editProduct = exports.addProduct = exports.getRandomProducts = exports.getPrints = exports.getPaintings = exports.getAllProducts = void 0;
 const db_1 = __importDefault(require("../db"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
+dotenv_1.default.config();
+const serverURL = process.env.SERVER_URL || 'http://localhost:8080';
 const getAllProducts = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const products = yield (0, db_1.default)('products');
@@ -54,3 +59,92 @@ const getRandomProducts = (req, res) => __awaiter(void 0, void 0, void 0, functi
     }
 });
 exports.getRandomProducts = getRandomProducts;
+const addProduct = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { title, size, price, type } = req.body;
+    console.log(req.file);
+    const newProduct = {
+        title,
+        image: `${serverURL}/images/${req.file.filename.replace(path_1.default.extname(req.file.filename), '.webp')}`,
+        size,
+        priceInCents: price * 100,
+        type
+    };
+    try {
+        const products = yield (0, db_1.default)("products");
+        const foundProduct = products.find(product => product.title === title);
+        if (!foundProduct) {
+            yield (0, db_1.default)("products").insert(newProduct);
+            return res.status(201).send("Product created!");
+        }
+        else {
+            return res.status(400).send("Error saving product");
+        }
+    }
+    catch (_e) {
+        return res.status(400).send("Error saving product");
+    }
+});
+exports.addProduct = addProduct;
+const editProduct = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { title, size, price, type, inStock } = req.body;
+    const { id } = req.params;
+    try {
+        const product = yield (0, db_1.default)("products").where({ id });
+        const foundProduct = product[0];
+        if (req.file && foundProduct.image.startsWith(serverURL)) {
+            fs_1.default.unlink("public" + foundProduct.image.replace(serverURL, ''), (err) => {
+                if (err) {
+                    console.log(err);
+                }
+            });
+        }
+        const editedProduct = {
+            title: title && title !== foundProduct.title ? title : foundProduct.title,
+            image: req.file ? `${serverURL}/images/${req.file.filename.replace(path_1.default.extname(req.file.filename), '.webp')}` : foundProduct.image,
+            size: size && size !== foundProduct.size ? size : foundProduct.size,
+            priceInCents: price && price !== Number(foundProduct.priceInCents) ? Number(price) * 100 : Number(foundProduct.priceInCents),
+            type: type && type !== foundProduct.type ? type : foundProduct.type,
+            inStock: inStock && inStock !== foundProduct.inStock ? inStock : foundProduct.inStock
+        };
+        console.log(editedProduct);
+        yield (0, db_1.default)("products").update(editedProduct).where({ id });
+        return res.status(200).send("Product Edited!");
+    }
+    catch (e) {
+        return res.status(404).send("Product not found.");
+    }
+});
+exports.editProduct = editProduct;
+const deleteProduct = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { id } = req.params;
+    try {
+        const product = yield (0, db_1.default)("products").where({ id });
+        const foundProduct = product[0];
+        const orders = yield (0, db_1.default)('orders');
+        const productExistsInOrder = [];
+        orders.forEach((order) => {
+            if (order.products.includes(foundProduct.id)) {
+                productExistsInOrder.push(true);
+            }
+            else {
+                productExistsInOrder.push(false);
+            }
+        });
+        if (productExistsInOrder.some((el) => el === true)) {
+            return res.status(400).send("Cannot delete product: The product exists in one or more order. The order(s) must be fulfilled and deleted before you can delete this product.");
+        }
+        else {
+            fs_1.default.unlink("public" + foundProduct.image.replace(serverURL, ''), (err) => {
+                if (err) {
+                    console.log(err);
+                }
+            });
+            yield (0, db_1.default)('products').delete().where({ id });
+            return res.status(200).send("Product successfully deleted.");
+        }
+    }
+    catch (e) {
+        return res.status(404).send("Product not found.");
+    }
+});
+exports.deleteProduct = deleteProduct;
