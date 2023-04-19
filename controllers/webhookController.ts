971c29908ff5bdcf9724f1a01b92b45f73043260
glitcher93 <nigeljdsouza@gmail.com>
2023-04-19 19:48:13 -0400

    added email functionality, and added an additional event check for successful refund

--- controllers/webhookController.ts
+++ controllers/webhookController.ts
@@ -3,12 +3,17 @@ import dotenv from 'dotenv';
 import { Request, Response } from 'express';
 import db from '../db';
 import { IItem } from '../utils/interfaces';
+import { Options } from 'nodemailer/lib/mailer';
+import { TemplateOptions } from 'nodemailer-express-handlebars';
+import transporter from '../utils/transporter';
 
 dotenv.config();
 
 const secretKey = process.env.STRIPE_SECRET_KEY as string;
 
-const webhookSecret = process.env.WEBHOOK_SECRET as string
+const webhookSecret = process.env.WEBHOOK_SECRET as string;
+
+const serverURL = process.env.SERVER_URL as string || 'http://localhost:8080';
 
 const stripe = new Stripe(secretKey, { apiVersion: '2022-11-15' });
 
@@ -46,6 +51,7 @@ export const webhook = async (req: Request, res: Response) => {
                 subtotal: data.amount_subtotal,
                 total: data.amount_total,
                 name: data.customer_details.name,
+                email: data.customer_details.email,
                 addressLineOne: data.customer_details.address.line1,
                 addressLineTwo: data.customer_details.address.line2,
                 city: data.customer_details.address.city,
@@ -61,16 +67,72 @@ export const webhook = async (req: Request, res: Response) => {
                     if (item.type === 'Painting') {
                         try {
                             await db('products').where({id: item.id}).update({inStock: false});
-                            res.status(200).json({received: true}).end()
                         } catch (err) {
                             return res.status(400).json(err);
                         }
                     }
                 })
+
+                const mailOptions: Options & TemplateOptions = {
+                    from: '"Inspiredbuthellatired" <inspiredbuthellatired@gmail.com>',
+                    to: data.customer_details.email,
+                    bcc: 'alyssamallone@hotmail.com',
+                    subject: `Thank You for Your Purchase! (Order #${customer.metadata.orderId})`,
+                    template: 'successful_purchase',
+                    context: {
+                        items,
+                        logo: `${serverURL}/images/logo.webp`,
+                        orderId: customer.metadata.orderId,
+                        name: data.customer_details.name,
+                        email: data.customer_details.email,
+                        addressLineOne: data.customer_details.address.line1,
+                        addressLineTwo: data.customer_details.address.line2,
+                        city: data.customer_details.address.city,
+                        state: data.customer_details.address.state,
+                        postalCode: data.customer_details.address.postal_code,
+                        phoneNumber: data.customer_details.phone,
+                        total: data.amount_total
+                    }
+                }
+                transporter.sendMail(mailOptions, (error, info) => {
+                    if (error) {
+                        console.log(error)
+                    }
+                    res.status(200).json({received: true}).end()
+                })
             } catch (err) {
                 return res.status(400).json(err);
             }
-            
+        } catch (err) {
+            return res.status(400).json(err);
+        }
+    }
+
+    if (eventType === 'charge.refunded') {
+        try {
+            const paymentIntentId = data.payment_intent;
+
+            const order = await db("orders").where({paymentIntentId});
+
+            const foundOrder = order[0];
+
+            const mailOptions: Options & TemplateOptions = {
+                from: '"Inspiredbuthellatired" <inspiredbuthellatired@gmail.com>',
+                to: data.customer_details.email,
+                bcc: 'alyssamallone@hotmail.com',
+                subject: `Order #${foundOrder.orderId} Cancelled and Refunded`,
+                template: 'successful_refund',
+                context: {
+                    logo: `${serverURL}/images/logo.webp`
+                }
+            }
+            transporter.sendMail(mailOptions, async (error, info) => {
+                if (error) {
+                    console.log(error)
+                }
+                await db("orders").delete().where({paymentIntentId});
+                res.status(200).json({canceled: true}).end()
+            })
         } catch (err) {
             return res.status(400).json(err);
         }
--- dist/controllers/webhookController.js
+++ dist/controllers/webhookController.js
@@ -16,9 +16,11 @@ exports.webhook = void 0;
 const stripe_1 = __importDefault(require("stripe"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const db_1 = __importDefault(require("../db"));
+const transporter_1 = __importDefault(require("../utils/transporter"));
 dotenv_1.default.config();
 const secretKey = process.env.STRIPE_SECRET_KEY;
 const webhookSecret = process.env.WEBHOOK_SECRET;
+const serverURL = process.env.SERVER_URL || 'http://localhost:8080';
 const stripe = new stripe_1.default(secretKey, { apiVersion: '2022-11-15' });
 const webhook = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const sig = req.headers['stripe-signature'];
@@ -48,6 +50,7 @@ const webhook = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
                 subtotal: data.amount_subtotal,
                 total: data.amount_total,
                 name: data.customer_details.name,
+                email: data.customer_details.email,
                 addressLineOne: data.customer_details.address.line1,
                 addressLineTwo: data.customer_details.address.line2,
                 city: data.customer_details.address.city,
@@ -62,17 +65,70 @@ const webhook = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
                     if (item.type === 'Painting') {
                         try {
                             yield (0, db_1.default)('products').where({ id: item.id }).update({ inStock: false });
-                            res.status(200).json({ received: true }).end();
                         }
                         catch (err) {
                             return res.status(400).json(err);
                         }
                     }
                 }));
+                const mailOptions = {
+                    from: '"Inspiredbuthellatired" <inspiredbuthellatired@gmail.com>',
+                    to: data.customer_details.email,
+                    bcc: 'alyssamallone@hotmail.com',
+                    subject: `Thank You for Your Purchase! (Order #${customer.metadata.orderId})`,
+                    template: 'successful_purchase',
+                    context: {
+                        items,
+                        logo: `${serverURL}/images/logo.webp`,
+                        orderId: customer.metadata.orderId,
+                        name: data.customer_details.name,
+                        email: data.customer_details.email,
+                        addressLineOne: data.customer_details.address.line1,
+                        addressLineTwo: data.customer_details.address.line2,
+                        city: data.customer_details.address.city,
+                        state: data.customer_details.address.state,
+                        postalCode: data.customer_details.address.postal_code,
+                        phoneNumber: data.customer_details.phone,
+                        total: data.amount_total
+                    }
+                };
+                transporter_1.default.sendMail(mailOptions, (error, info) => {
+                    if (error) {
+                        console.log(error);
+                    }
+                    res.status(200).json({ received: true }).end();
+                });
             }
             catch (err) {
                 return res.status(400).json(err);
             }
+        }
+        catch (err) {
+            return res.status(400).json(err);
+        }
+    }
+    if (eventType === 'charge.refunded') {
+        try {
+            const paymentIntentId = data.payment_intent;
+            const order = yield (0, db_1.default)("orders").where({ paymentIntentId });
+            const foundOrder = order[0];
+            const mailOptions = {
+                from: '"Inspiredbuthellatired" <inspiredbuthellatired@gmail.com>',
+                to: data.customer_details.email,
+                bcc: 'alyssamallone@hotmail.com',
+                subject: `Order #${foundOrder.orderId} Cancelled and Refunded`,
+                template: 'successful_refund',
+                context: {
+                    logo: `${serverURL}/images/logo.webp`
+                }
+            };
+            transporter_1.default.sendMail(mailOptions, (error, info) => __awaiter(void 0, void 0, void 0, function* () {
+                if (error) {
+                    console.log(error);
+                }
+                yield (0, db_1.default)("orders").delete().where({ paymentIntentId });
+                res.status(200).json({ canceled: true }).end();
+            }));
         }
         catch (err) {
             return res.status(400).json(err);
